a14610bb06b48865d8332c1d9f3b17ea1a8c68d5
paswan31 <165304160+paswan31@users.noreply.github.com>
2024-03-29 21:51:20 +0530

    web

---
+++ index.js
@@ -0,0 +1,24 @@
+function a(){
+    document.getElementById("pp").src="./images/img2.avif"
+}
+function b(){
+    document.getElementById("pp").src="./images/img3.avif"
+}
+function c(){
+    document.getElementById("pp").src="./images/img4.avif"
+}
+function d(){
+    document.getElementById("pp").src="./images/img5.avif"
+}
+function e(){
+    document.getElementById("pp").src="./images/img6.avif"
+}
+function f(){
+    document.getElementById("pp").src="./images/img7.avif"
+}
+function g(){
+    document.getElementById("pp").src="./images/img8.avif"
+}
+function h(){
+    document.getElementById("pp").src="./images/img9.avif"
+}
